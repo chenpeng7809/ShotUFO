003f7d41c75943f2e35950240a0fd855aef27411
chenpeng7809 <chenpeng>
2015-02-06 21:25:31 +0800

    战斗UI

---
+++ Assets/GameEngine/GameView/FightUI.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FightUI : AUIBase
+{
+		public override void OnOpen (object param)
+		{
+				base.OnOpen (param);
+		}
+
+		public override void OnClose (object param)
+		{
+				base.OnClose (param);
+		}
+		
+}
